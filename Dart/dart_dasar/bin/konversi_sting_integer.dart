20b8726482e52bc7496324e1d1621fea7aadd30e
gilang_permana <gilanghyper27@gmail.com>
2021-12-24 23:25:08 +0700

    menambar print pada konversi

--- Dart/dart_dasar/bin/konversi_sting_integer.dart
+++ Dart/dart_dasar/bin/konversi_sting_integer.dart
@@ -11,6 +11,12 @@ void main(){
   var doubleFromInt = inputInt.toDouble();
   var intFromDouble = inputDouble.toInt();
 
+  print(doubleFromInt);
+  print(intFromDouble);
+
   var stringFromInt  = inputInt.toString();
   var stringFromDouble = inputDouble.toString();
+
+  print(stringFromInt);
+  print(stringFromDouble);
 }
