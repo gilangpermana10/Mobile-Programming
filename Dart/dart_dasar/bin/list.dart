3a5a2aca7fb3040db29e89bfc3ca9a100e4ea863
gilang_permana <gilanghyper27@gmail.com>
2021-12-29 22:49:18 +0700

    Create list.dart

---
+++ Dart/dart_dasar/bin/list.dart
@@ -0,0 +1,25 @@
+
+void main(){
+
+  List<int> bilanganGenap = [];
+  var listString = <String>[];
+
+  bilanganGenap.add(2);
+  bilanganGenap.add(4);
+  bilanganGenap.add(6);
+  bilanganGenap.add(8);
+
+  listString.add('gilang');
+  listString.add('permana');
+  listString.add('riza');
+
+  listString[2]='sayangku';
+  listString.removeAt(1);
+
+  print(bilanganGenap);
+  print(bilanganGenap.length);
+
+  print(listString);
+  print(listString.length);
+   
+}
