2dc511cb043b8bdb908e52041b8e0eaf01d4b59f
gilang_permana <gilanghyper27@gmail.com>
2021-12-29 21:56:19 +0700

    menambah File Pembelajaran Operator

---
+++ Dart/dart_dasar/bin/operator_logika.dart
@@ -0,0 +1,14 @@
+// &&,||,!
+void main(){
+  var nilaiAkhir = 90;
+  var nilaiAbsen = 50;
+
+  var isNilaiAkhirGood = nilaiAkhir >= 70;
+  var isNilaiAbsenGood = nilaiAbsen >= 70;
+
+  print(isNilaiAkhirGood);
+  print(isNilaiAbsenGood);
+
+  var lulus = isNilaiAkhirGood || isNilaiAbsenGood;
+  print(lulus);
+}
---
+++ Dart/dart_dasar/bin/operator_penugasan.dart
@@ -0,0 +1,14 @@
+void main(){
+  var a = 10;
+
+  a += 10;
+
+  print(a);
+
+  var i =0;
+  var j = ++i;
+  print('ini adalah i ' + i.toString());
+  print('ini adalah j ' + j.toString());
+ 
+
+}
