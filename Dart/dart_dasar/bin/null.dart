1f6a1cc0e87eba44278b988bfebfcb02640064a1
gilang_permana <gilanghyper27@gmail.com>
2022-01-09 23:59:22 +0700

    Menambah beberpaa file

---
+++ Dart/dart_dasar/bin/null.dart
@@ -0,0 +1,9 @@
+void main(){
+  int number;
+  number = 10;
+  int? number2;
+
+  print(number);
+  print(number2);
+
+}
---
+++ Dart/dart_dasar/bin/tenaryOperator.dart
@@ -0,0 +1,18 @@
+
+void main(List<String> args) {
+  
+  var nilai = 75;
+  // String ucapan;
+
+  // if(nilai >= 75){
+  //   ucapan = 'Selamat anda lulus';
+  // }else{
+  //   ucapan = 'Silahkan coba lagi';
+  // }
+
+  // print(ucapan);
+
+  var ucapan = nilai >= 75 ? 'Selamat anda lulus' : 'Silahkan coba lagi';  
+  print(ucapan);
+  
+}
