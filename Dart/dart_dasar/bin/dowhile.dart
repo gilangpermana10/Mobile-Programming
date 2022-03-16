ad740ab1418f71b6485f145c6b1173e7d7ab37c5
gilang_permana <gilanghyper27@gmail.com>
2022-03-16 21:57:32 +0700

    add some files

---
+++ Dart/dart_dasar/bin/dowhile.dart
@@ -0,0 +1,10 @@
+import 'dart:developer';
+
+void main(){
+  var counter=1;
+  
+  do{
+    print('perulangan ke-$counter');
+    counter++;
+  }while(counter<=10);
+}
---
+++ Dart/dart_dasar/bin/for_in.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var names = <String>['gilang','permana','loss'];
+
+  // for(var i=0;i<names.length;i++){
+  //   print(names[i]);
+  // }
+
+  for(var value in names){
+    print(value);
+  }
+}
